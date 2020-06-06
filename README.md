3aff2a53620de0623d802dcf5e4e8f0b7d492283
rylandking <ryland@users.noreply.github.com>
2020-06-06 14:53:47 -0600

    First commit... Totally should've been committing the whole time. lol

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-This is a starter template for [Learn Next.js](https://nextjs.org/learn).
+This is a starter template for a [Next.js](https://nextjs.org/learn) blog.
+
+Differences from Next.js tutorial blog are:
+-- Routes are category/id
+-- You can show and hide posts by category
+
+To get started, simply create a folder under /posts. The name of this folder is the "category".
+
+Inside the category folder, create relevant articles via Markdown.
+
+And viola! You have a blog.
--- pages/index.js
+++ pages/index.js
@@ -1,20 +1,58 @@
 import Head from 'next/head'
+import Link from 'next/link'
+import { useState } from "react"
 import Layout, { siteTitle } from '../components/layout'
+import Date from '../components/date'
 import utilStyles from '../styles/utils.module.css'
+import { getSortedPostsData } from '../lib/posts'
 
-export default function Home() {
+export default function Home({ allPostsData }) {
+  // Delcare what category should be shown
+  const [viewCategory, setCategory] = useState('all');
+
   return (
     <Layout home>
       <Head>
         <title>{siteTitle}</title>
       </Head>
       <section className={utilStyles.headingMd}>
-        <p>[Your Self Introduction]</p>
-        <p>
-          (This is a sample website - you’ll be building a site like this in{' '}
-          <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
-        </p>
+        <p>Ryland's Blog</p>
+      </section>
+      <section>
+        <button onClick={() => setCategory(viewCategory === 'neck-pain' ? 'all' : 'neck-pain')}>Neck Pain</button>
+        <button onClick={() => setCategory(viewCategory === 'low-back-pain' ? 'all' : 'low-back-pain')}>Low Back Pain</button>
+        <button onClick={() => setCategory(viewCategory === 'chronic-pain' ? 'all' : 'chronic-pain')}>Chronic Pain</button>
+        <button onClick={() => setCategory(viewCategory === 'pelvic-health' ? 'all' : 'pelvic-health')}>Pelvic Health</button>
+      </section>
+      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+        <h2 className={utilStyles.headingLg}>Blog</h2>
+        <ul className={utilStyles.list}>
+          {allPostsData.map(({ id, category, date, title, image }) => (
+            <li className={utilStyles.listItem} key={id} style={{ 
+              display: (viewCategory === category || viewCategory === 'all') ? "block":"none"
+            }}>
+            <Link href="/[category]/[id]" as={`/${category}/${id}`}>
+              <a>{title}</a>
+            </Link>
+            <br />
+            <small className={utilStyles.lightText}>
+              <Date dateString={date} />
+            </small>
+            <br />
+            <img src={image} alt={id}/>
+          </li>
+          ))}
+        </ul>
       </section>
     </Layout>
   )
+}
+
+export async function getStaticProps() {
+  const allPostsData = getSortedPostsData()
+  return {
+    props: {
+      allPostsData
+    }
+  }
 }
