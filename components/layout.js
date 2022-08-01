faa699769aa6edba5568a5150912c147ab46d303
arobbins-vermeer <arobbins@vermeer.com>
2022-08-01 09:17:36 -0500

    add sidebar

--- components/layout.js
+++ components/layout.js
@@ -55,11 +55,31 @@ export default function Layout({ children, home }) {
             </>
           )}
         </header>
-    </div>
-      
+    </div>    
 
-      
-      <main>{children}</main>
+    <div className='row'>
+      <div className='nine columns'>
+        <main>{children}</main>
+      </div>
+      <div className='three columns sidebar'>
+        <sidebar>
+            <section className="sidebarItem">
+                  <h3 className="sidebarTitle">An Aside</h3>
+                  <p>
+                      It can often be a wonder what the Platform Team does in a world with so many "platforms". Well, we hope that this handy website will not only answer that question for you but also provide many other insights and information you will find useful!
+                  </p>
+              </section>
+          
+              <section className="sidebarItem">
+                      <h3 className="sidebarTitle">Contributing Content</h3>
+                      <p>
+                          To contribute content to this site please refer to the README on <a href = "https://github.com/vermeer-corp/pt-swa-comm-hub-v2">GitHub</a>
+                      </p>
+              </section> 
+        </sidebar> 
+      </div>
+    </div>
+
 
     </div>
   )
